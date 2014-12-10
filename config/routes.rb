ccc44e46147a3e1776d61e843bd06ca665f46fe4
alivin <540355718@qq.com>
2014-12-10 16:12:12 +0800

    simple a top

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :microposts
+
+  resources :users
+
+  #root 'application#hello'
+  root 'users#index'
+  get 'goodbye' => 'application#goodbye'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
